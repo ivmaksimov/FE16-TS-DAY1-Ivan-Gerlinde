014fa0955d927976858d6424d2f9f8421b4cd8a8
Gerlinde <gerlinde@sandmair.com>
2022-06-09 13:57:24 +0200

    changes

--- gerlinde/basic_02/ex02.js
+++ gerlinde/basic_02/ex02.js
@@ -1,9 +1,9 @@
-
+"use strict";
 let user = {
     fname: "Gerlinde",
     lname: "Sandmair"
 };
 console.log(user);
 console.log(`${user.fname} und ${user.lname}`);
-let user_pos = (document.getElementById("user") as HTMLElement); 
-user_pos.innerHTML  = `${user.fname} und ${user.lname}`;
+let user_pos = document.getElementById("user");
+user_pos.innerHTML = `${user.fname} und ${user.lname}`;
